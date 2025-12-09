d23432b5f53d0942ccb411bd46875d4d6ad538f4
Oleh Stashchyshyn <o.s.dubbie@gmail.com>
2025-12-10 01:54:40 +0200

    update index.js

--- js/index.js
+++ js/index.js
@@ -15,14 +15,15 @@
         subtitle.style.opacity = '1';
       }, 4000);
       
-      const introVideo = document.getElementById('intro-video');
-      if (window.innerWidth <= 720) {
-        video.innerHTML = `
-      <source src="entrance_video_mobile.mp4" type="video/mp4">
-    `;
-        video.load();
-        video.play();
-      }
+const introVideo = document.getElementById('intro-video');
+
+if (window.innerWidth <= 720) {
+  introVideo.innerHTML = `
+    <source src="./img/entrance_video_mobile.mp4" type="video/mp4">
+  `;
+  introVideo.load();
+  introVideo.play();
+}
 
       const burger = document.getElementById('burger');
       const mobileNav = document.getElementById('mobileNav');
